d148eee76513deb3789b64ce7ff276b4f8d0f1e1
samdi18 <41270492+samdi18@users.noreply.github.com>
2020-07-12 01:18:53 +0600

    Initial Setup
    - Installed dependencies
    - Created config files for mongodb connection
    - Created server to listen to incoming requests
    - Connected to database
    - Created API routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//to connect to the mongodb database
+connectDB();
+
+//Initialize middleware for post json requests
+app.use(express.json());
+
+// The routes for the API of auth
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
+// for ports
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
